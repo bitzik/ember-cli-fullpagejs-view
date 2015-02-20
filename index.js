2a1a3684986110bbd3287c1503b7d54904856525
leo jPod <leo.jpod@gmail.com>
2015-02-20 22:32:48 +0100

    adjusting the BrocFile dependencies

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-fullpagejs-view'
+  name: 'ember-cli-fullpagejs-view',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import({
+      development: 'bower_components/fullpage.js/jquery.fullPage.js',
+      prod: 'bower_components/fullpage.js/jquery.fullPage.min.js',
+    });
+
+    app.import('bower_components/fullpage.js/jquery.fullPage.css');
+  }
 };
