6db543aee2235ae49a83fbb3bcba4ed2fcc9e99f
leo jPod <leo.jpod@gmail.com>
2015-02-27 20:20:54 +0100

    restoring the defautl blueprint name and trying to correctly handle the bower dependency

---
+++ blueprints/ember-cli-fullpagejs-view/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  description: 'ember-cli-fullpagejs-view',
+
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  // included: function(app) {
+  //   this._super.included(app);
+
+  //   app.import({
+  //     development: 'bower_components/fullpage.js/jquery.fullPage.js',
+  //     prod: 'bower_components/fullpage.js/jquery.fullPage.min.js',
+  //   });
+
+  //   app.import('bower_components/fullpage.js/jquery.fullPage.css');
+  // },
+
+  afterInstall: function() {
+    return this.addBowerPackageToProject('fullpage.js', '2.5.7');
+  }
+};
