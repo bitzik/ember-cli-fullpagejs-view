5017c2d45425ab350bdee6d0100137cd0f7f11a5
leo jPod <leo.jpod@gmail.com>
2015-03-09 10:58:28 +0100

    if only npm link was working well on window...

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    //app.import({
-    //  development: 'bower_components/fullpage.js/jquery.fullPage.js',
-    //  prod: 'bower_components/fullpage.js/jquery.fullPage.min.js',
-    //});
-    //
-    //app.import('bower_components/fullpage.js/jquery.fullPage.css');
+    app.import({
+      development: 'bower_components/fullpage.js/jquery.fullPage.js',
+      prod: 'bower_components/fullpage.js/jquery.fullPage.min.js',
+    });
+
+    app.import('bower_components/fullpage.js/jquery.fullPage.css');
   }
 };
