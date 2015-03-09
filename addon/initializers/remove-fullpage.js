76ad276f49aa2497145ad2589322121ba5aa237c
leo jPod <leo.jpod@gmail.com>
2015-03-09 12:15:08 +0100

    if only npm link was working well on window...

--- addon/initializers/remove-fullpage.js
+++ addon/initializers/remove-fullpage.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export function initialize() {
   //Ember.$(document).ready(function () {
     //Ember.Logger.debug('no fullpage by default!');
-    Ember.$('body').fullpage();
+    var fakeDiv = Ember.$('<div>');
+    fakeDiv.fullpage();
     //it is silly but the easiest way to remove the fullpage.js default initialization is by loading it first...
     Ember.$.fn.fullpage.destroy('all');
+    fakeDiv.remove();
   //});
   //Ember.$.fn.fullpage.destroy('all');
 }
