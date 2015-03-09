1b8beaf8bc575a2843340213383d4123110a6733
leo jPod <leo.jpod@gmail.com>
2015-03-09 10:32:14 +0100

    correcting initializer names

--- addon/initializers/remove-fullpage.js
+++ addon/initializers/remove-fullpage.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export function initialize() {
   Ember.$(document).ready(function () {
     //Ember.Logger.debug('no fullpage by default!');
-    Ember.$('body').fullpage();
+    $('body').fullpage();
     //it is silly but the easiest way to remove the fullpage.js default initialization is by loading it first...
-    Ember.$.fn.fullpage.destroy('all');
+    $.fn.fullpage.destroy('all');
   });
   //Ember.$.fn.fullpage.destroy('all');
 }
